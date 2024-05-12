8063cff637d9815f15f9b6c43bd5432587aefbc5
goeun kim <rlarhdms6828@naver.com>
2024-05-12 21:30:58 +0900

    [DFS/BFS] unsolved

---
+++ DFS,BFS/2178 미로탐색/미로탐색.js
@@ -0,0 +1,8 @@
+let fs = require('fs');
+let filePath = process.platform === 'linux' ? '/dev/stdin' : '/input.txt';
+let input = fs
+  .readFileSync(__dirname + filePath)
+  .toString()
+  .split('\n');
+
+let [N, M] = input.shift().split(' ');
