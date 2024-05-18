fbfb0c3716ee77939fb8819efedb3fa543e32555
goeun kim <rlarhdms6828@naver.com>
2024-05-18 21:20:46 +0900

    [dfs/bfs] 2178 미로탐색

--- DFS,BFS/2178 미로탐색/미로탐색.js
+++ DFS,BFS/2178 미로탐색/미로탐색.js
@@ -5,45 +5,46 @@ let input = fs
   .toString()
   .split('\n');
 
-let [N, M] = input.shift().split(' ');
+let [N, M] = input.shift().split(' ').map(Number);
 
-console.log(N, M);
-
-let visitied = [];
+let visitied = [...new Array(N)].map(() => Array(M).fill(false)); // 방문 체크용
 let needvisite = [];
 let map = [];
+let ans = [];
 
-// 미로모양 이중배열로 만들기
+//미로모양 이중배열로 만들기
 for (let i = 0; i < N; i++) {
   map[i] = input[i].split('').map(Number);
 }
 
-needvisite.push([0, 0]);
-map[0][0] = 0;
+let current = [0, 0, 1]; // 열, 행, 움직인칸수
+let next_x = [-1, 1, 0, 0]; // 상 하 좌 우
+let next_y = [0, 0, -1, 1]; // 상 하 좌 우
 
-//dfs로 풀어야겠다
+needvisite.push(current);
+visitied[0][0] = true; // 방문표시
+
 while (needvisite.length > 0) {
-  let target = needvisite.shift();
-  if (!visitied.includes(target)) {
-    visitied.push(target);
-    for (let i = 0; i < N; i++) {
-      for (let j = 0; j < M; j++) {
-        if (
-          map[i][j] == 1 &&
-          ((target[0] + 1 == i && target[1] == j) ||
-            (target[0] - 1 == i && target[1] == j) ||
-            (target[0] == i && target[1] - 1 == j) ||
-            (target[0] == i && target[1] + 1 == j))
-        ) {
-          if (i == N - 1 && j == M - 1) {
-            console.log(visitied.length + 1);
-          }
-          map[i][j] = 0;
-          needvisite.unshift([i, j]);
-        }
-      }
+  // console.log(needvisite);
+  let [x, y, move] = needvisite.shift();
+  if (x == N - 1 && y == M - 1) {
+    ans.push(move); //(N,M)에 도달한 경우
+    break;
+  }
+
+  for (let i = 0; i < 4; i++) {
+    let nx = x + next_x[i];
+    let ny = y + next_y[i];
+    let nm = move + 1;
+    // 주어진 map안에 들어가고 && 1인 곳이며 && 아직 방문 안 한 곳
+    if (nx >= 0 && nx < N && ny >= 0 && ny < M && map[nx][ny] == 1 && !visitied[nx][ny]) {
+      current = [nx, ny, nm]; // 방문가능한 곳 초기화
+      // console.log(current);
+      needvisite.push(current); // 큐에 넣어줌
+      visitied[nx][ny] = true; // 방문표시
     }
   }
 }
-// console.log(map);
-// console.log(visitied);
+
+// console.log(ans);
+console.log(Math.min(ans));
