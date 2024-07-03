66da60e7d68571c3af98502fdf4e5b7e3db98685
goeun kim <rlarhdms6828@naver.com>
2024-07-04 02:08:21 +0900

    [DP] 1463 1로 만들기

--- 다이나믹 프로그래밍/1463 1로 만들기/1463.js
+++ 다이나믹 프로그래밍/1463 1로 만들기/1463.js
@@ -5,32 +5,28 @@ let N = +fs
   .toString()
   .trim();
 
-let turns = [];
-let turn = 0;
+let dp = [];
 
-function recursion(n) {
-  if (n == 1) {
-    turns.push(turn);
-    return;
-  }
+dp[1] = 0;
+dp[2] = dp[1] + 1;
+dp[3] = dp[1] + 1;
+// dp[4] = dp[4-1] + 1 || dp[4/2] + 1
+// dp[5] = dp[5-1] + 1
+// dp[6] = dp[6-1] + 1 || dp[6/2] + 1 || dp[6/3] + 1
 
-  if (n % 3 == 0 && n % 2 == 0) {
-    recursion(n / 3, turn++);
-    recursion(n / 2, turn++);
+for (let i = 4; i <= N; i++) {
+  if (i % 3 == 0) {
+    dp[i] = Math.min(dp[i - 1] + 1, dp[i / 3] + 1);
   }
-  if (n % 3 == 1 && n % 2 == 1) {
-    recursion((n - 1) / 3, (turn += 2));
-    recursion((n - 1) / 2, (turn += 2));
+  if (i % 2 == 0) {
+    dp[i] = Math.min(dp[i - 1] + 1, dp[i / 2] + 1);
   }
-  if (n % 3 == 0) recursion(n / 3, turn++);
-  if (n % 2 == 0) recursion(n / 2, turn++);
-  if (n % 3 == 1) {
-    recursion((n - 1) / 3, (turn += 2));
+  if (i % 2 == 0 && i % 3 == 0) {
+    dp[i] = Math.min(dp[i - 1] + 1, dp[i / 2] + 1, dp[i / 3] + 1);
   }
-  if (n % 2 == 1) {
-    recursion((n - 1) / 2, (turn += 2));
+  if (i % 2 !== 0 && i % 3 !== 0) {
+    dp[i] = dp[i - 1] + 1;
   }
 }
 
-recursion(N);
-console.log(turns);
+console.log(dp[N]);
