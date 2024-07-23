1ce874c01562a68657ca9b0159096c6f9a59c979
goeun kim <rlarhdms6828@naver.com>
2024-07-23 21:09:00 +0900

    2751 수 정렬하기

---
+++ CLASS/class2/2751 수 정렬하기 2/2751.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+let filePath = process.platform === 'linux' ? '/dev/stdin' : '/input.txt';
+let input = fs
+  .readFileSync(__dirname + filePath)
+  .toString()
+  .trim()
+  .split('\n');
+
+let N = +input.shift();
+input.sort((a, b) => a - b);
+
+console.log(input.join('\n'));
