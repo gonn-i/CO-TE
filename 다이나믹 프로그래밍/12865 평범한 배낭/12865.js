f16c328ba2c7e93a969837a82a6239f3b408c844
goeun kim <rlarhdms6828@naver.com>
2024-09-08 21:41:11 +0900

    12865 평벙한 배낭

--- 다이나믹 프로그래밍/12865 평범한 배낭/12865.js
+++ 다이나믹 프로그래밍/12865 평범한 배낭/12865.js
@@ -8,17 +8,23 @@ let input = fs
 
 let things = [];
 const [N, K] = input.shift().split(' ').map(Number);
+
 for (let i = 0; i < N; i++) {
   things[i] = input[i].split(' ').map(Number);
 }
 
-let dp = [...new Array(K + 1)].map((_) => 0);
+let dp = [...new Array(N + 1)].map((_) => new Array(K + 1).fill(0));
 
-for (let i = 0; i < N; i++) {
-  let weight = things[i][0];
-  if (weight <= K) {
-    dp[weight] = things[i][1];
+for (let i = 1; i <= N; i++) {
+  let [weight, value] = things[i - 1];
+  for (let j = 1; j <= K; j++) {
+    // if 최대하중 7 & weight: 6 이라면  => 6, 7 넣을 수 있음
+    if (j - weight >= 0) {
+      dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight] + value); // 이전 값 or 하나 더 넣기
+    } else {
+      dp[i][j] = dp[i - 1][j]; // 넣을 수 없다면, 이전 값 유지
+    }
   }
 }
 
-console.log(dp);
+console.log(Math.max(...dp[N]));
